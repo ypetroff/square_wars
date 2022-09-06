055bad4f7cf32958d37eda78be759dd776570621
Yordan Petrov <yordan.petroff@gmail.com>
2022-09-07 00:07:37 +0300

    modify level1

--- src/js/views/level_1.js
+++ src/js/views/level_1.js
@@ -4,7 +4,6 @@ const level_1Template = () => html `
 <section class="screen level1">
 <h1>This is level_1</h1>
 <p>Lorem ipsum dolor sit amet consecteturd? Magnam reprehenderit, officiis ab molestiae obcaecati odio atque iusto hic maiores, voluptates deleniti ea dolorum, recusandae suscipit ad laudantium dolore?</p>
-<button>Start Game</button>
 </section>
 `;
 
