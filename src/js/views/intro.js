53c54f560b2a6c2c38ae1d247be1665300adf179
Yordan Petrov <yordan.petroff@gmail.com>
2022-09-07 22:16:25 +0300

    intro view design and buttons positioning

--- src/js/views/intro.js
+++ src/js/views/intro.js
@@ -2,13 +2,17 @@ import { html } from "../lib.js";
 
 const introTemplate = (onStart) => html`
   <section class="screen intro">
-    <h1>How it all staarted</h1>
+    <h1>How it all started</h1>
     <p>
       Lorem ipsum dolor sit amet consecteturd? Magnam reprehenderit, officiis ab
       molestiae obcaecati odio atque iusto hic maiores, voluptates deleniti ea
       dolorum, recusandae suscipit ad laudantium dolore?
     </p>
-    <button @click=${onStart}>Start Game</button>
+    <p>
+      <h2>Instructions</h2>
+    </p>
+    <button class="button game" @click=${onStart}>Start Game</button>
+    <button class="button survival">Survival Mod</button>
   </section>
 `;
 
